aa68626375c93b38bfac49417e96e793f08ae389
Anaikana <131258102+Anaikana@users.noreply.github.com>
2023-06-02 18:52:00 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head> <!--toutes les informations exploitées de l'extérieur (seo, navigateur, synthétiseur vocal ...)-->
+    <meta charset="UTF-8"> <!--pour que le site soit compris partout dans le monde, notamment avec é,è etc-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- navigateurs pris en charge-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--largeur d'écran, sans zoom avec la notion scale-->
+    
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="stylescss/style.css">
+        <title>Cdemoiselles - Coaching sportif et conseil en image</title> <!--titre de la page lors ouverture dans le navigateur-->
+       
+</head>
+
+<body>
+<?php 
+?>
---
+++ nav.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid px-4">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">Hidden brand</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aPropos.php">À propos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aPropos.php">Prestations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="priseRDV.php">Prendre rdv</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="blog.php">Le blog</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Rechercher</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<?php ?>
